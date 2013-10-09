3481a60c375d0bbfd6a9acf7296acfcc298ce9de
mtakagi <ts-makoto.a.takagi@mail.rakuten.com>
2013-10-09 16:55:52 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnimeAntenna::Application.config.secret_token = 'bfff5ac4aa7f31040f3b5615d56ad0ff93f3fd9d386af558520f86ad95b3d03866076f301cae0f9e9707323928446cfbebac7b0a57a32053a992e9737f2e870f'
